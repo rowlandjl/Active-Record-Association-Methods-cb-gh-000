c53750eeabe36cefe4007b11c088e93982195025
Jessica Rowland <rowlandjl@vcu.edu>
2018-08-03 19:45:56 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -4,11 +4,12 @@ class Genre < ActiveRecord::Base
 
   def song_count
     # return the number of songs in a genre
-    self.songs.size 
+    self.songs.size
   end
 
   def artist_count
     # return the number of artists associated with the genre
+    self.artists.size 
   end
 
   def all_artist_names
